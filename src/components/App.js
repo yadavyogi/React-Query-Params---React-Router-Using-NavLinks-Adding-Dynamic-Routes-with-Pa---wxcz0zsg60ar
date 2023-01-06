8711a01e2abfe025c6d94b1275dafea27c41d266
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-01-06 22:25:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import '../styles/App.css';
 
-const App = () => {
-  //Write your code here
+
+function App() {
+  const queryParams = new URLSearchParams(window.location.search)
+  const id = queryParams.get("id")
+  const name = queryParams.get("name")
+  const designation = queryParams.get("designation")
+
+  return (
+    <div className="App">
+      <p>Employee{id}{name}{designation}</p>
+    </div>
+  )
 }
 
 export default App;
